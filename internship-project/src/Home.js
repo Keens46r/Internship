cb0e3b8a1f28035df815124d4d71a269e5dfcf4b
Keens46r <160424@student.horizoncollege.nl>
2022-12-05 16:07:29 +0100

    Api
    Api data fetched using json and displayed on website

--- internship-project/src/Home.js
+++ internship-project/src/Home.js
@@ -1,9 +1,28 @@
 const Home = () => {
-    const title = 'Home Page';
-    
+    const title = 'Random joke';
+
+    const api_url = ('https://official-joke-api.appspot.com/random_joke')
+    async function getData(){
+        const response = await fetch(api_url);
+        const data = await response.json();
+        const { setup, punchline } = data;
+        
+        document.getElementById('setup').textContent = setup;
+        document.getElementById('punchline').textContent = punchline;
+   
+    }
+
+    //Interval set to fetch new random joke
+    // setInterval(getData, 15000);
+
+    getData();
     return (  
         <div className="home">
-            <h2>{title}</h2>
+            <h3>{title}</h3>
+            <p>
+                Joke: <span  class="joke" id="setup"></span><br/>
+                Answer: <span class="joke" id="punchline"></span><br/>
+            </p>
         </div>
     );
 }
